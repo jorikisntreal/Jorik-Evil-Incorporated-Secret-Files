5f4c45e4d5828c8cfc565551c243cf9a8826cff5
jorikisntreal <118592039+jorikisntreal@users.noreply.github.com>
2024-09-07 10:21:14 +0700

    added peter griffin app
    it's what is says

---
+++ peter griffin app/go.mod
@@ -0,0 +1,7 @@
+module example.com/m
+
+go 1.23.0
+
+require github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
+
+require golang.org/x/sys v0.1.0 // indirect
---
+++ peter griffin app/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pkg/browser"
+)
+
+func main() {
+
+	fmt.Println("Dam")
+
+	browser.OpenURL("https://www.wallpaperflare.com/static/940/129/189/family-guy-peter-griffin-peter-griffin-wallpaper.jpg")
+
+}
